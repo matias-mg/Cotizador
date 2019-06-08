b386ee3dda55b983c75801cc15833cb7d9196110
Chross <matias@gmail.com>
2019-06-08 16:33:47 -0400

    I've finished Formulary component and now i'll send it as a prop

--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,44 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Formulario extends Component {
-    render() {
-        return (
-            <form className="cotizar-auto">
-                <div className="campo">
-                    <label>Marca</label>
-                    <select name="marca" >
-                        <option value="americano">Americano</option>
-                        <option value="europeo">Europeo</option>
-                        <option value="asiatico">Asiatico</option>
-                    </select>
-                </div>
+  //Crear refs
+  brandRef = React.createRef();
+  yearRef = React.createRef();
+  basicPlanRef = React.createRef();
+  fullPlanRef = React.createRef();
 
-                <div className="campo">
-                    <label>Año</label>
-                    <select name="year">
-                        <option value="2018">2018</option>
-                        <option value="2017">2017</option>
-                        <option value="2016">2016</option>
-                        <option value="2015">2015</option>
-                        <option value="2014">2014</option>
-                        <option value="2013">2013</option>
-                        <option value="2012">2012</option>
-                        <option value="2011">2011</option>
-                        <option value="2010">2010</option>
-                        <option value="2009">2009</option>
-                        <option value="2008">2008</option>
-                    </select>
-                </div>
-                <div className="campo">
-                    <label>Plan:</label>
-                    <input type="radio" name="plan" value="basico"/> Básico
-                    <input type="radio" name="plan" value="completo"/> Completo
-                </div>
+  handle = (e) => {
+    e.preventDefault();
 
-                <button type="submit" className="boton">Cotizar</button>
-            </form>
-        );
+    //Obtener radioButton seleccionado
+    const plan = this.basicPlanRef.current.checked ? 'basico':'completo';
+
+    //Objeto para imprimir auto
+    const infoAuto = {
+      brand: this.brandRef.current.value,
+      year: this.yearRef.current.value,
+      plan: plan 
     }
+
+    console.log(infoAuto);
+  }
+
+  render() {
+    return (
+      <form className="cotizar-auto" onSubmit={this.handle}>
+        <div className="campo">
+          <label>Marca</label>
+          <select name="marca" ref={this.brandRef}>
+            <option value="americano">Americano</option>
+            <option value="europeo">Europeo</option>
+            <option value="asiatico">Asiatico</option>
+          </select>
+        </div>
+
+        <div className="campo">
+          <label>Año</label>
+          <select name="year" ref={this.yearRef}>
+            <option value="2018">2018</option>
+            <option value="2017">2017</option>
+            <option value="2016">2016</option>
+            <option value="2015">2015</option>
+            <option value="2014">2014</option>
+            <option value="2013">2013</option>
+            <option value="2012">2012</option>
+            <option value="2011">2011</option>
+            <option value="2010">2010</option>
+            <option value="2009">2009</option>
+            <option value="2008">2008</option>
+          </select>
+        </div>
+        <div className="campo">
+          <label>Plan:</label>
+          <input type="radio" name="plan" value="basico" ref={this.basicPlanRef} /> Básico
+          <input type="radio" name="plan" value="completo" ref={this.full} /> Completo
+        </div>
+
+        <button type="submit" className="boton">
+          Cotizar
+        </button>
+      </form>
+    );
+  }
 }
 
 export default Formulario;
